67996171fe2cd3c07a41d6c38281e509feb8a77f
Sean Macdonald <sean@crazyhorsecoding.com>
2015-09-18 17:11:12 -0400

    file permissions on bash scripts, and ensure services are deamonized

--- services/nodejs/run.sh
+++ services/nodejs/run.sh
@@ -2,7 +2,6 @@
 
 DIR=$(git rev-parse --show-toplevel)
 source $DIR/vars.sh
-
 servicename=$1
 malleability=$2
 containername=$project-$appname-$branch-$servicename
@@ -10,7 +9,7 @@ imagename=$imagebase-$servicename:$branch
 IS_AMBASSADOR="$(jshon -e services -e $servicename -e ambassador -u < $appdef)"
 
 if [ "$malleability" == "hard" ]; then
-    docker run -P \
+    docker run -P -d \
         --name $containername \
         --label io.sjc.orchestra.version="$ORCHESTRA_VERSION" \
         --label io.sjc.orchestra.project="$project" \
