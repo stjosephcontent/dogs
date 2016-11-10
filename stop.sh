3abd1db1257cc16ef02f3e78cbed7c21490d37d1
Sean Macdonald <sean@crazyhorsecoding.com>
2016-11-09 21:38:38 -0500

    update to 1.0.5

--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$(git rev-parse --show-toplevel)
-source $DIR/vars.sh
-
-for service in $services; do
-    docker stop $project-$appslug-$branch-$service && docker rm $project-$appslug-$branch-$service
-done
